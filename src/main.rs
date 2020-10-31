adf04096c6cbc630f99060e12d4607575cf4e00b
Romeu Vieira <romeu.bizz@gmail.com>
2020-10-31 22:28:05 +0000

    Using monolith to download URLs

--- src/main.rs
+++ src/main.rs
@@ -28,21 +28,10 @@ impl Feed {
 
 impl ToString for Feed {
     fn to_string(&self) -> String {
-        let empty_str = "".to_owned();
-        let empty_bool = false;
-        let empty_date = Utc.timestamp(0, 0);
-
-        return format!("rssurl = \"{}\"\nurl = \"{}\"\ntitle = \"{}\"\nlastmodified = \"{}\"\nis_rtl = \"{}\"\netag = \"{}\"",
-            self.rssurl.as_ref().unwrap_or_else(|| &empty_str),
-            self.url.as_ref().unwrap_or_else(|| &empty_str),
-            self.title.as_ref().unwrap_or_else(|| &empty_str),
-            self.lastmodified.as_ref().unwrap_or_else(|| &empty_date),
-            self.is_rtl.as_ref().unwrap_or_else(|| &empty_bool),
-            self.etag.as_ref().unwrap_or_else(|| &empty_str),
-        );
+        return format!("{:?}", self);
     }
 }
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 struct FeedItem {
     id: Option<u32>,
     guid: Option<String>,
@@ -111,8 +100,8 @@ fn db_get_feed_items(dbcon: &sqlite::Connection) -> Vec<FeedItem> {
 
     dbcon
         .iterate("SELECT * FROM rss_item", |pairs| {
-            let feed = FeedItem::from_tuple_array(pairs);
-            feed_item_list.push(feed);
+            let feed_item = FeedItem::from_tuple_array(pairs);
+            feed_item_list.push(feed_item);
 
             true
         })
@@ -121,13 +110,140 @@ fn db_get_feed_items(dbcon: &sqlite::Connection) -> Vec<FeedItem> {
     return feed_item_list;
 }
 
+// fn myfunc<'a>(mut strings: impl Iterator<Item=&'a str>, key: &'a str) -> bool {
+//         strings.any(|item| key.contains(item))
+// }
+
+const URL_BLACKLIST: [&'static str; 2] = ["https://www.youtube.com/", "https://nitter.net/"];
+
+fn is_url_in_blacklist<'a>(url: &'a str) -> bool {
+    for blacklisted in &URL_BLACKLIST {
+        if url.contains(blacklisted) {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+fn str_sanitize(s: String) -> String {
+    return s
+        .replace(" ", "-")
+        .replace(",", "")
+        .replace(":", "")
+        .replace("(", "")
+        .replace(")", "")
+        .replace("'", "")
+        .replace("*", "");
+}
+
+pub trait StringExtensions {
+    fn sanitize(&mut self) -> String;
+}
+
+impl StringExtensions for String {
+    fn sanitize(&mut self) -> String {
+        return self
+            .replace(" ", "-")
+            .replace(",", "")
+            .replace(":", "")
+            .replace("(", "")
+            .replace(")", "")
+            .replace("'", "")
+            .replace("*", "")
+            .replace("|", "")
+            .replace(";", "")
+            .replace("‘", "")
+            .replace("`", "")
+            .replace("`", "")
+            .replace("…", "")
+            .replace("...", "")
+            .replace("<", "")
+            .replace(">", "")
+            .replace("&", "")
+            .replace("--", "-")
+            .replace("--", "-")
+            .replace("--", "-");
+    }
+}
+
 fn main() {
-    let connection = sqlite::open("/home/romeu/.local/share/newsboat/cache.db").unwrap();
+    let args = clap::App::new("newsboat-archiver")
+        .version("1.0")
+        .author("Romeu Vieira <romeu.bizz@gmail.com>")
+        .about("Archive Newsboat DB information")
+        .arg(
+            clap::Arg::with_name("File")
+                .short("f")
+                .long("file")
+                .value_name("FILE")
+                .help("Database file")
+                .takes_value(true),
+        )
+        .arg(
+            clap::Arg::with_name("Output")
+                .short("d")
+                .long("directory")
+                .value_name("DIRECTORY")
+                .help("Output directory")
+                .takes_value(true),
+        )
+        .get_matches();
+
+    let arg_db = args.value_of("File").expect("File argument not valid!");
+    let arg_directory = args
+        .value_of("Output")
+        .expect("Directory argument is not valid!");
+
+    let dir_metadata = std::fs::metadata(arg_directory).unwrap();
+    if !dir_metadata.is_dir() {
+        println!("Invalid path: not a directory!\n");
+        std::process::exit(-1);
+    }
+
+    // e.g.: /home/romeu/.local/share/newsboat/cache.db
+    let connection = sqlite::open(arg_db).unwrap();
 
     let feed_list = db_get_feed(&connection);
     let feed_item_list = db_get_feed_items(&connection);
 
-    for item in feed_item_list {
-        println!("{}\n", item.to_string());
+    for feed in feed_list {
+        let feed_url = feed.url.unwrap();
+        let feed_rssurl = feed.rssurl.unwrap();
+        let feed_title = feed.title.unwrap().sanitize();
+
+        if is_url_in_blacklist(&feed_url) {
+            continue;
+        }
+
+        // create directory
+        let feed_dir = format!("{}/{}", arg_directory, feed_title);
+        std::fs::create_dir(&feed_dir).expect("Directory could not be created.");
+
+        let feed_item_list_clone = feed_item_list.clone();
+
+        let items = feed_item_list_clone
+            .iter()
+            .filter(|item| {
+                item.feedurl.clone().unwrap().contains(&feed_url)
+                    || item.feedurl.clone().unwrap().contains(&feed_rssurl)
+            })
+            .collect::<Vec<_>>();
+
+        for item in items {
+            let title = item.title.clone().unwrap().sanitize();
+
+            let url = item.url.as_ref().unwrap();
+            let monolith = format!("monolith -s {} > {}/{}.html", url, feed_dir, title);
+
+            println!("Command to execute: {}", monolith);
+
+            // use `output()` to block
+            std::process::Command::new("sh")
+                .arg("-c")
+                .arg(monolith)
+                .output()
+                .expect("Failed to execute monolith.");
+        }
     }
 }
